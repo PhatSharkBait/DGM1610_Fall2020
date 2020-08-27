1b7171df1358786080c59047f913bfab04c73ac2
Cameron <Cameronjudd98@gmail.com>
2020-08-27 15:38:16 -0600

    Added Script and Rider to project

---
+++ cyborgShrimp/Assets/NumberData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("Your Script is working");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

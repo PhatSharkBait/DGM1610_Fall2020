38c7137bea9bada4d3b4f26bd72ffb25afd27f8f
Cameron <Cameronjudd98@gmail.com>
2020-08-28 13:24:01 -0600

    Started Personal Project
    Messed with lighting and created a simple script rotating a ball on the y-axis

---
+++ PersonalProject/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public Rigidbody ball;
+    public float spinSpeed;
+    private float yRotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRotation+= spinSpeed;
+        ball.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}

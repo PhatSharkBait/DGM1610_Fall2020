77083e170b95e499d3b5acf2a8dbe135b22d98b7
Cameron <Cameronjudd98@gmail.com>
2020-09-01 20:10:09 -0600

    Created basic HUD with health counter that decreases on player input

--- cyborgShrimp/Assets/Scripts/PlayerHealth.cs
+++ cyborgShrimp/Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int maxHealth;
+    public int currentHealth;
+    public TextMesh hitpointCounter;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonUp("Submit"))
+        {
+            currentHealth--;
+        }
+
+        hitpointCounter.text = currentHealth.ToString();
     }
 }

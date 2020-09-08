c645d4fd2d75562f450301cb4e59ed29af26c326
Cameron <Cameronjudd98@gmail.com>
2020-09-08 13:38:18 -0600

    Created Gem w/ basic texture, destroyed on collision

---
+++ PersonalProject/Assets/Scripts/GemBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other1)
+    {
+        Debug.Log("ouchie");
+        Destroy(gameObject);
+    }
+}

51d171f52eca0f38502198a14d0b8d246d50bb16
Cameron <Cameronjudd98@gmail.com>
2020-09-09 22:06:20 -0600

    Created Speed Power Up

---
+++ cyborgShrimp/Assets/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public MoverScript player;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        player.speed = 20;
+    }
+}

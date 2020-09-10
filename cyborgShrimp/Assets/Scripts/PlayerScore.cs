489dd552061957d130d92c9d039b4d3b97b6db79
Cameron <Cameronjudd98@gmail.com>
2020-09-10 15:54:05 -0600

    Created Ammo Power Up
    Added Ammo variable to Player Score script

--- cyborgShrimp/Assets/Scripts/PlayerScore.cs
+++ cyborgShrimp/Assets/Scripts/PlayerScore.cs
@@ -9,15 +9,6 @@ public class PlayerScore : MonoBehaviour
     public string playerName = "Bobby";
 
     private string password = "secrets";
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int ammo;
 }

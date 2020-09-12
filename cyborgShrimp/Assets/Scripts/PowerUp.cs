4eb7125ce7d1cb78a144854ca315ba7d864964ae
Cameron <Cameronjudd98@gmail.com>
2020-09-11 19:40:05 -0600

    Wrote score power up

---
+++ cyborgShrimp/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PowerUp : MonoBehaviour
+{
+    public TextMesh pointCounter;
+    public int score;
+
+    private void Awake()
+    {
+        score = 0;
+        pointCounter.text = "Score: " + score;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score += 10;
+        pointCounter.text = "Score: " + score;
+    }
+}

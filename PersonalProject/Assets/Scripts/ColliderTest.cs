ea981c1dbb67eb7ff3283f4b7788fc30d65e4a4b
Cameron <Cameronjudd98@gmail.com>
2020-09-14 15:43:23 -0600

    Found strange glitch using collider test
    left over collider called "player" stays at spawn.

---
+++ PersonalProject/Assets/Scripts/ColliderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour
+{
+    public Collider playerCollider;
+
+    private void Awake()
+    {
+        playerCollider = GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject);
+    }
+}

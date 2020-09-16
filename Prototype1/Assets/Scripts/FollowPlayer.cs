06e1cd407f501998446fb9f1a4a18426248cb0ff
Cameron <Cameronjudd98@gmail.com>
2020-09-15 19:46:53 -0600

    Create with Code 1.2/1.3

---
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 6, -8);
+
+    private void Update()
+    {
+        //offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    
+    void Update()
+    {
+       // Move the vehicle forward
+       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}

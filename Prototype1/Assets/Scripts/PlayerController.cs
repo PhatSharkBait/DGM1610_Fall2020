125548f6692151f13f515d3421682abc64f49bd9
Cameron <Cameronjudd98@gmail.com>
2020-09-16 19:44:37 -0600

    Create with Code 1.4

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
+    //Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     
     void Update()
     {
-       // Move the vehicle forward
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+       //Move the vehicle forward
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       //turn the vehicle
+       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+       
     }
 }

abdc9eaecea022bb91459e40dc7e3f633441ab53
Cameron <Cameronjudd98@gmail.com>
2020-09-24 21:33:50 -0600

    Create with Code 2.3

---
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+   public GameObject[] animalPrefabs;
+   private float spawnRange = 15f;
+   private float spawnPosZ = 30f;
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.S))
+      {
+         int animalIndex = Random.Range(0, animalPrefabs.Length);
+         Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
+         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+      }
+   }
+}

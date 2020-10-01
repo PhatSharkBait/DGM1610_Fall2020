f2dd933f16af91aff37edf7bcbf4b9926664539f
Cameron <Cameronjudd98@gmail.com>
2020-09-30 19:14:11 -0600

    Create with Code 3 setup
    also finished Unit 2 Quiz

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        playerRb.AddForce(Vector3.up * 690);
+    }
+}

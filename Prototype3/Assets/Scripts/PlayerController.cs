9ea65a1bd9cb7403594d700398b90085a6d8665f
Cameron <Cameronjudd98@gmail.com>
2020-10-03 15:50:04 -0600

    Create with Code 3.1, 3.2, 3.3

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isGrounded;
+    public bool gameOver;
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
     }
 
-    private void Start()
+    private void Update()
     {
-        playerRb.AddForce(Vector3.up * 690);
+        if (Input.GetButtonDown("Jump") && isGrounded && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        } else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
     }
 }

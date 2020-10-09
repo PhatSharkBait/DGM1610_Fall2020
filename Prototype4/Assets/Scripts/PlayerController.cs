7dc4e9789f67fe6b2b51aca7e8d0e146d8279290
Cameron <Cameronjudd98@gmail.com>
2020-10-09 16:10:50 -0600

    Create with Code 4.1

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 15.0f;
+    public GameObject cameraPivot;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(cameraPivot.transform.forward * (speed * forwardInput));
+    }
+}

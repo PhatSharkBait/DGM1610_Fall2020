c90a3ad1f8297b1d45a5377d4ee18b51edcf4cc8
Cameron <Cameronjudd98@gmail.com>
2020-10-15 17:35:34 -0600

    Added Int Data Script and applied to gems

---
+++ PersonalProject/Assets/ScriptableObjects/IntData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+
+    public void ReplaceValue(int number)
+    {
+        value = number;
+    }
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
+    }
+}

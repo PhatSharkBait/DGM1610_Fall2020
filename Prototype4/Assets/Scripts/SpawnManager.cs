78f67b0e6a57d1a54ed7c5b1cd782ced1aa06f78
Cameron <Cameronjudd98@gmail.com>
2020-10-17 14:39:11 -0600

    Create with Code 4.2

---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemy;
+    private float spawnRange = 9;
+    private void Start()
+    {
+ 
+        Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+}

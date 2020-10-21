e4eec100acd95a837dc421d2ca0e825a99a6c602
Cameron <Cameronjudd98@gmail.com>
2020-10-20 23:19:58 -0600

    Create with Code 4.4

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject powerup;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+
     private void Start()
     {
- 
-        Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        Instantiate(powerup, GenerateSpawnPosition(), powerup.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            Instantiate(powerup, GenerateSpawnPosition(), powerup.transform.rotation);
+            waveNumber++; 
+            SpawnEnemyWave(waveNumber);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -17,5 +34,13 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+    
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+        }
     }
 }

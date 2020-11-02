0d60bb36e2bd985536891da28413508342c5e78c
Cameron <Cameronjudd98@gmail.com>
2020-11-02 16:51:57 -0700

    More Work on Player Rotation

--- MayanKickball/Assets/Scripts/Player.cs
+++ MayanKickball/Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     private float vertical;
     private float horizontal;
     private float moveSpeed = 10f;
-    private float rotationSpeed = 2f;
+    private float rotationSpeed = 50f;
     private Vector3 moveDir;
     private Quaternion rotation;
 
@@ -33,15 +33,16 @@ public class Player : MonoBehaviour
     //rotate in toward given direction
     private void rotatePerson(Vector3 lookDir)
     {
-        Vector3 newRotation = Vector3.RotateTowards(transform.forward, lookDir.normalized, rotationSpeed, 0.0f);
-        transform.rotation = Quaternion.LookRotation(newRotation);
+        var lookAngle = Mathf.Rad2Deg*(Math.Asin(lookDir.x) + Math.Acos(lookDir.z));
+        print(lookAngle);
+        //var qTo = Quaternion.Euler(0, lookAngle, 0);
+        //var lookSpeed = rotationSpeed * Time.deltaTime;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, qTo, lookSpeed);
     }
     
     //move in facing direction
     private void movePerson(Vector3 moveDir)
     {
-        //var xDir = Math.Cosh(transform.rotation.y);
-        //Debug.Log(xDir);
         Vector3 movement = (moveDir * (Time.deltaTime * moveSpeed));
         transform.position += movement;
     }

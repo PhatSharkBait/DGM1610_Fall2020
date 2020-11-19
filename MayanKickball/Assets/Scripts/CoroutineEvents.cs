4f224e841e69cd7a754e01214a023c020ac92c02
Cameron <Cameronjudd98@gmail.com>
2020-11-19 15:50:30 -0700

    Added Timer using coroutines

---
+++ MayanKickball/Assets/Scripts/CoroutineEvents.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineEvents : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime, repeatHoldTime = 0.25f;
+    public int counter = 3;
+
+    public void RunCoroutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    private IEnumerator Coroutine()
+    {
+        startEvent.Invoke();
+        
+        yield return new WaitForSeconds(holdTime);
+
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+            counter--;
+        }
+        
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}

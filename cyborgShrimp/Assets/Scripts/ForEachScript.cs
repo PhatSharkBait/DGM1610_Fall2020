6d79dfa2b1248d1bc5f3996904ee40c6e05d0531
Cameron <Cameronjudd98@gmail.com>
2020-12-08 15:42:01 -0700

    Created for each loop

---
+++ cyborgShrimp/Assets/Scripts/ForEachScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachScript : MonoBehaviour {
+    public List<Collectable> collectedObjects;
+
+    public void Start() {
+        foreach (var food in collectedObjects) {
+            Debug.Log(food);
+        }
+    }
+}
